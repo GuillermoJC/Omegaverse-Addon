96ff6436aee4897c8b8ef85fe1fce5022e50c1fc
GuillermoJCV <memo92975@gmail.com>
2025-01-18 15:16:38 -0600

    Añade los tipos del addon para evitar errores de typing

---
+++ BP/scripts/core/types/GiveMePointsFormValues.ts
@@ -0,0 +1,10 @@
+import { SkillsPointsOptionsResults } from "../constants/form_results"
+
+type GiveMePointsFormValues = [
+    SkillsPointsOptionsResults,
+    number
+]
+
+export type {
+    GiveMePointsFormValues
+}
