5ac689a8e505557a276ac759609accf2b285c4b6
GuillermoJCV <memo92975@gmail.com>
2025-01-18 15:08:04 -0600

    agrega el punto de entrada a los scripts del addon

---
+++ BP/scripts/index.ts
@@ -0,0 +1,33 @@
+import { system, world } from '@minecraft/server'
+import { runtime } from "./core/runtime"
+import PlayerEvents from "./core/events/PlayerEvents"
+import WorldEvents from './core/events/WorldEvents'
+import { MinecraftBlockIds } from './core/constants/block_ids'
+
+
+world.afterEvents.itemUse.subscribe((e) => PlayerEvents.afterUseAdminKey(e))
+
+world.afterEvents.playerBreakBlock.subscribe((e) => PlayerEvents.afterBrakeAMobSpawn(e), { blockTypes: [MinecraftBlockIds.MobSpawner] })
+
+
+// TODO: Crear el mob de Alfa
+// Al matar un Alfa debería dar puntos de AlfaKillerPoints
+
+world.afterEvents.worldInitialize.subscribe(() => WorldEvents.onWorldInitialize())
+
+world.afterEvents.itemCompleteUse.subscribe(e => PlayerEvents.afterItemCompleteUse(e))
+
+world.afterEvents.playerSpawn.subscribe(e => PlayerEvents.afterPlayerSpawnEvent(e))
+
+
+//El script que se va a ejecutar por tick
+system.run(function runnable() {
+    system.run(runnable)
+    runtime()
+})
+
+//TODO:
+
+//Arreglar el sistema de puntos de minado : core/util/MathFunctions.ts ; core/controllers/PlayerController
+
+//Hacer gametest para cada funcionalidad
