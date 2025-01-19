c04376488af3ddfa179e099b97213da130fca57e
GuillermoJCV <memo92975@gmail.com>
2025-01-19 14:17:24 -0600

    Agrega los archivos para registrar nuevos gametests

---
+++ BP/scripts/core/gametests/GametestCreator.ts
@@ -0,0 +1,12 @@
+import * as Gametest from "@minecraft/server-gametest"
+import { GametestCreateArguments } from "../types/GametestCreateArguments"
+
+class GametestCreator {
+
+    static registerAllTests() {
+
+    }
+
+}
+
+export default GametestCreator
---
+++ BP/scripts/core/types/GametestCreateArguments.ts
@@ -0,0 +1,10 @@
+type GametestCreateArguments = {
+    group: string,
+    name: string,
+    maxTicks?: number,
+    structureName: string
+}
+
+export type {
+    GametestCreateArguments
+}
