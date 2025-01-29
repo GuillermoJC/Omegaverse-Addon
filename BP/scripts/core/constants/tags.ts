cb6b0521771c362ddeb66bfe9d1920f783209aea
GuillermoJCV <memo92975@gmail.com>
2025-01-29 16:54:39 -0600

    Cambia el id de las tags para evitar problemas de compatibilidad con otros addons

--- BP/scripts/core/constants/tags.ts
+++ BP/scripts/core/constants/tags.ts
@@ -1,13 +1,13 @@
 enum PlayerTags {
-    firstSpawn = "omegaverse-z:first_spawn_check",
-    noClassPlayer = "omegaverse-z:no_class_player",
-    omegaPlayer = "omegaverse-z:omega_player",
-    betaPlayer = "omegaverse-z:beta_player",
-    alfaPlayer = "omegaverse-z:alfa_player"
+    firstSpawn = "omegaverse_z:first_spawn_check",
+    noClassPlayer = "omegaverse_z:no_class_player",
+    omegaPlayer = "omegaverse_z:omega_player",
+    betaPlayer = "omegaverse_z:beta_player",
+    alfaPlayer = "omegaverse_z:alfa_player"
 }
 
 enum OmegaTags {
-    hasPeriod = "hasPeriod"
+    hasPeriod = "omegaverse_z:hasPeriod"
 }
 
 
